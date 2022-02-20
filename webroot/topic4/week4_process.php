c25f1343251a13966d286e5e5138ac92c2f3b6bd
yaseen rashid <yaseenrashid001@outlook.com>
2022-02-20 16:47:39 +0000

    exercise 4 week 4

---
+++ webroot/topic4/week4_process.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta lang="en">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="exercise4.css">
+    <link rel="stylesheet" href="reset.css">    
+    <title>Week 4 - Form Results</title>
+  </head>
+  <body>
+      <section class="form_outer_container">
+          <section class="form_inner_container">        
+              <header>
+                  <h1>
+                      GET Method Data
+                  </h1>
+              </header>
+          </section>
+          <section class="form_inner_container">
+              <header>
+                  <h1>
+                      POST Method Data
+                  </h1>
+              </header>
+          </section>
+      </section>
+      <script src="week4.js"></script>
+  </body>
+</html>
