260c0ae9886ad730fc78900fadbc5cc28bf7d101
yaseen rashid <yaseenrashid001@outlook.com>
2022-02-26 17:12:22 +0000

    css file created

--- miniproject/index.php
+++ miniproject/index.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="reset.css">
 	<meta charset="UTF-8">
 	<title>My Portfolio</title>
 </head>
 <body>
+<nav>
+	<ul>
+		<li>
+			<a>
+				LOGO TO BE PLACED
+			</a>
+		</li>
+		<li>
+			<a href="#aboutMe">About Me</a>
+		</li>
+		<li>
+			<a href="#experience">Experience</a>
+		</li>
+		<li>
+			<a href="#skills">Skills</a>
+		</li>
+		<li>
+			<a href="#education">Education</a>
+		</li>
+		<li>
+			<a href="#projects">Projects</a>
+		</li>
+		<li>
+			<a href="#blog">Blog</a>
+		</li>
+	</ul>
+</nav>
 
 
 
