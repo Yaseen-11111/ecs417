7d18405a110ee306e6285b812bff9ff23fd8d613
yaseen rashid <yaseenrashid001@outlook.com>
2022-03-12 22:13:16 +0000

    mini phase 1

--- webroot/miniproject/index.php
+++ webroot/miniproject/index.php
@@ -11,7 +11,7 @@
 	<a>
 		LOGO TO BE PLACED
 	</a>
-	<ul class="navBar">
+	<ul class="nav-bar">
 		<li>
 			<a href="#aboutMe">About Me</a>
 		</li>
@@ -32,18 +32,99 @@
 		</li>
 	</ul>
 </nav>
-<article class="headerArticle">
+<article class="content-article">
 	<header>
 		<h1>
-			My Portfolio
+			Welcome to my portfolio
 		</h1>
 	</header>
-	<section>
-		This is my asdjsadjaspodasds as dj aspdj apisjd asd
-	</section>
+	<section id="#aboutMe" class="content-container-1 row">
+		<section class="col-fit content">
+			<header>
+				<h2>
+					About me
+				</h2><hr>
+				<h3>
+					Computer Science Undergraduate studying at Queen Mary University of London
+				</h3>
+			</header>
+			<p>
+				I am one to accept challenges and enjoy solving complex problems which is why chose to pursue a career in
+				Software Engineering, where I have a particular field in mind, being AI and machine learning, as the idea
+				of interacting and creating an Artificial mind is fascinating and exhilarating. For me to achieve this goal
+				I need to complete my bachelor's degree in Computer Science and focus on my side projects by working with AI
+				and machine learning. Then work towards securing my placement year in a technology company that interest me
+				such as, Google, Microsoft, etc. so that I can gain on hand experience working with professionals.
+			</p><br>
+			<p>
+				Apart from Computer Science, I'm an athletic person where I'm a part of Queen Mary Rugby team and go gym
+				regularly alongside boxing to keep myself fit and healthy, as well as me enjoying sport which is another
+				aspect of who I am.
+			</p>
+		</section>
+		<section class="col-fit content">
+			<figure class="profile-picture">
+				<img src="winterwonderland.jpg">
+				<figcaption>
+					Aspiring machine learning and AI specialist
+				</figcaption>
+			</figure>
+
+		</section>
+	</section><hr>
+
+	<section id="experience" class="content-container row">
+		<section class="col-fit content">
+			<header>
+				<h2>
+					Experience
+				</h2><hr>
+			</header>
+		</section>
+	</section><hr>
+
+	<section id="skills" class="content-container row">
+		<section class="col-fit content">
+			<header>
+				<h2>
+					Skills
+				</h2><hr>
+			</header>
+		</section>
+	</section><hr>
+
+	<section id="education" class="content-container row">
+		<section class="col-fit content">
+			<header>
+				<h2>
+					Education
+				</h2><hr>
+			</header>
+		</section>
+	</section><hr>
+
+	<section id="projects" class="content-container row">
+		<section class="col-fit content">
+			<header>
+				<h2>
+					Projects
+				</h2><hr>
+			</header>
+		</section>
+	</section><hr>
+
+	<section id="blog" class="content-container row">
+		<section class="col-fit content">
+			<header>
+				<h2>
+					My Blog
+				</h2><hr>
+			</header>
+		</section>
+	</section><hr>
 </article>
+<footer>
 
-
-
+</footer>
 </body>
 </html>
