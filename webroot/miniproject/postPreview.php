91ce08dec6496b2eb037f63dcb8f68ae94b09120
yaseen rashid <yaseenrashid001@outlook.com>
2022-05-12 21:56:42 +0100

    asd

--- webroot/miniproject/postPreview.php
+++ webroot/miniproject/postPreview.php
@@ -9,8 +9,8 @@
 	</header>
 	<section class="blog-content-container row">
 		<form id="addEntry" action="addEntry.php" method="post">
-			<input id="title" name="Title" type="text" placeholder="Title" class="blog-input-box" maxlength="32">
-			<textarea id="desc" name="Description" placeholder="Enter your blog... " class="blog-input-box blog-textarea" ></textarea>
+			<input id="title" name="Title" type="text" placeholder="Title" class="blog-input-box" maxlength="32" readonly>
+			<textarea id="desc" name="Description" placeholder="Enter your blog... " class="blog-input-box blog-textarea" readonly></textarea>
 			<button id="submit" name="add_entry" type="submit" class="button-box submit">Post</button>
 			<button id="back" type="button" class="button-box clear">Back</button>
 		</form>
